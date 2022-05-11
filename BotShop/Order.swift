efab61935616837540f432101d0ab1339b888b5d
Tay <tmitsumori96@gmail.com>
2022-05-10 20:26:17 -0700

    View past orders

---
+++ BotShop/Order.swift
@@ -0,0 +1,15 @@
+//
+//  Order.swift
+//  BotShop
+//
+//  Created by Tay Mitsumori on 5/10/22.
+//
+
+import Foundation
+import UIKit
+
+struct Order {
+    var title: String
+    var image: UIImage
+    var items: [Item]
+}
---
+++ BotShop/OrderList.swift
@@ -0,0 +1,55 @@
+//
+//  OrderList.swift
+//  BotShop
+//
+//  Created by Tay Mitsumori on 5/10/22.
+//
+
+import Foundation
+import UIKit
+
+class OrderList: UIViewController {
+    var currentOrder: Order!
+    var orderItems: [Item] = []
+
+    let tableView =  UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        tableView.register(PastOrderCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        setUpTableView()
+    }
+
+    func setUpTableView(){
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
+
+}
+
+extension OrderList: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currentOrder.items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PastOrderCell
+            cell.accessoryType = .disclosureIndicator
+            cell.selectionStyle = .none
+        cell.setCellContents(item: currentOrder.items[indexPath.row])
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+
+}
