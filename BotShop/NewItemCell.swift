5797cbfe5f709fc018d46a66d6ad18d8f8ff8e58
Tay <tmitsumori96@gmail.com>
2022-05-10 19:53:16 -0700

    Basic table view

--- BotShop/NewItemCell.swift
+++ BotShop/NewItemCell.swift
@@ -9,6 +9,15 @@ import Foundation
 import UIKit
 
 class NewItemCell: UICollectionViewCell {
+    
+    var data: Item? {
+        didSet{
+            guard let data = data else { return }
+            image.image = data.image
+            title.text = data.title
+        }
+    }
+    
     let stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
@@ -37,9 +46,25 @@ class NewItemCell: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: .zero)
+        setup()
+    }
+    
+    func setup() {
+        contentView.addSubview(stackView)
+
+        stackView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.80).isActive = true
+        stackView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.70).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+
+        stackView.addArrangedSubview(image)
+        image.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 0.55).isActive = true
+
+        stackView.addArrangedSubview(title)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
 }
--- BotShop/NewOrderViewController.swift
+++ BotShop/NewOrderViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 extension NewOrderViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return data.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NewItemCell
         cell.backgroundColor = .green
+        cell.data = self.data[indexPath.row]
         return cell
     }
     
@@ -25,6 +26,15 @@ extension NewOrderViewController: UICollectionViewDataSource, UICollectionViewDe
 }
 
 class NewOrderViewController: UIViewController {
+    
+    let data = [
+        Item(title: "Respiratory", image: UIImage(named: "robot1")!),
+        Item(title: "Muscular", image: UIImage(named: "robot2")!),
+        Item(title: "Endocrine", image: UIImage(named: "robot3")!),
+        Item(title: "Excretory", image: UIImage(named: "robot4")!),
+        Item(title: "Lymphatic", image: UIImage(named: "robot5")!),
+        Item(title: "Nervous", image: UIImage(named: "robot6")!)
+    ]
     
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
